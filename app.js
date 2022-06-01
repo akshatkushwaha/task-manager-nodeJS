fc7da2c9aad9917e4a82a2e64f64af6ca8ae42b0
akshatkushwaha <akshat.official999@gmail.com>
2022-06-02 02:12:11 +0530

    server port change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
   res.status(404).send("Route does not exits");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
